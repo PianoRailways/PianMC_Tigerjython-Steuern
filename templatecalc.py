d304d0c605f155198e27e597f2d527a10e00814e
PianoRailways <git@pianorailways.tk>
2019-03-02 22:16:28 +0100

    Create templatecalc.py

---
+++ templatecalc.py
@@ -0,0 +1,20 @@
+regionname = inputString("Regionname")
+Owner = inputString("Regionowner")
+nmember = inputInt("Anzahl Mitglieder exkl. Owner")
+
+x1 = inputInt("x1")
+y1 = inputInt("y1")
+z1 = inputInt("z1")
+
+x2 = inputInt("x1")
+y2 = inputInt("y1")
+z2 = inputInt("z1")
+
+xlang = x1-x2
+hoch = sqrt((y1-y2+80)**2)
+zlang = z1-z2
+area = sqrt((xlang*zlang)**2)
+
+kosten = area*0.6+hoch*100+nmember*2750
+
+print("{{TJS|" + str(regionname) + "|" + str(Owner) + "|"+str(xlang)+"*"+str(zlang)+"->'''"+str(area)+"'''|"+str(hoch)+"|"+str(kosten)+"}}")
